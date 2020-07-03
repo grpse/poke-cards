a3bffecd6f4b8c4a91542e364a1a6c8c37ee8fac
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-07-03 09:50:39 -0300

    pokemons local query resolver to use data from cache before using from fetched ones of server

---
+++ src/graphql/resolvers/pokemons.ts
@@ -0,0 +1,69 @@
+import { Pokemon } from "../../@types/Pokemon"
+import { InMemoryCache, ApolloClient } from "apollo-boost"
+import { POKEMONS_LOCAL } from "../queries/PokemonsLocal"
+import { POKEMONS } from '../queries/Pokemons'
+import { CacheDataType } from "../createApolloClient"
+
+export async function pokemons(_ : any, { first } : { first : number }, { cache, client } : { cache : InMemoryCache, client : ApolloClient<any>}) {
+                    
+    let cachedData : CacheDataType | null = null
+
+    try {
+        cachedData = cache.readQuery({ query: POKEMONS_LOCAL, variables: { first } })
+
+        if (first === cachedData?.first) {
+            return cachedData.pokemons
+        }
+
+    } catch(e) {
+        console.log('Error on getting from cache', e)
+    }
+
+    try {
+        
+        const data : {
+            data: {
+                pokemons: Pokemon[]
+            },
+            loading: boolean
+        } = await client.query({
+            query: POKEMONS,
+            variables: { first }
+        })
+        
+        const fetchedPokemons = data?.data.pokemons
+        
+        if (cachedData) {
+            
+            const mergedPokemonsWithModifiedOnes = fetchedPokemons.map((fp, index) => {
+                if (cachedData?.pokemons[index]) {
+                    return cachedData?.pokemons[index]
+                } else {
+                    return fp
+                }
+            })
+
+            cache.writeData({
+                data: {
+                    pokemons: mergedPokemonsWithModifiedOnes,
+                    first
+                }
+            })
+
+            return mergedPokemonsWithModifiedOnes
+        } else {
+
+            cache.writeData({
+                data: {
+                    pokemons: fetchedPokemons,
+                    first
+                }
+            })
+
+            return fetchedPokemons
+        }
+        
+    } catch(e) {
+        console.log('Error fetching pokemons list:', e)
+    }
+}
