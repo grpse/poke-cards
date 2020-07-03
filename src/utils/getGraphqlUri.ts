47de6b6457fc2031d60af78df18bb9aa11853f06
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-07-03 17:21:05 -0300

    conditional server url

--- src/utils/getGraphqlUri.ts
+++ src/utils/getGraphqlUri.ts
@@ -1,5 +1,7 @@
 export function getGraphqlUri() {
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.GRAPHQL_SERVER_URL) {
+        return process.env.GRAPHQL_SERVER_URL
+    } else if (process.env.NODE_ENV === 'production') {
         return 'https://graphql-pokemon.now.sh'
     } else {
         return 'http://localhost:5000'
