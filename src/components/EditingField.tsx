730508915c51fe3de1b3abb7867a1d24d7f2812f
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-07-03 09:43:21 -0300

    add editing field file

---
+++ src/components/EditingField.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Chip, OutlinedInput, makeStyles, IconButton } from '@material-ui/core'
+import RemoveIcon from '@material-ui/icons/Remove'
+
+export type EditingFieldProps = {
+    isEditing: boolean
+    value: string
+    onChange(newValue : string): void
+    onRemove(): void
+}
+
+export function EditingField({ isEditing, value, onChange, onRemove } : EditingFieldProps) {
+    
+    const classes = useStyles()
+
+    return (
+        isEditing ?
+            <>
+                <OutlinedInput
+                    value={value}
+                    onChange={event => onChange(event.target.value)}
+                    placeholder='Type'
+                    classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput
+                    }} />
+                <IconButton onClick={() => onRemove()} className={classes.removeButton}>
+                    <RemoveIcon />
+                </IconButton>
+            </>
+            :
+            <Chip label={value} variant='outlined' />
+    )
+}
+
+const useStyles = makeStyles({
+    inputRoot: {
+        borderRadius: 30
+    },
+    inputInput: {
+        height: 0,
+        fontSize: '0.8125rem'
+    },
+    removeButton: {
+        color: 'red'
+    }
+})
