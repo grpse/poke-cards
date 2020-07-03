c49109fd7f03e6b9247ab6ef27826b47494f45c1
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-07-03 10:06:43 -0300

    init the app router and set local storage persistence

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { createApolloClient } from './graphql/createApolloClient'
+import AppRouter from './components/AppRouter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { client, setPersistence } = createApolloClient()
+
+export default function App() {
+	
+	const [ loaded, setLoaded ] = useState(false)
+
+	useEffect(() => {
+		setPersistence().finally(() => setLoaded(true))
+	}, [])
+
+	if (loaded) {
+		return (
+		  <ApolloProvider client={client}>
+			  <AppRouter />
+		  </ApolloProvider>
+		)
+	} else {
+		return null
+	}
 }
-
-export default App;
